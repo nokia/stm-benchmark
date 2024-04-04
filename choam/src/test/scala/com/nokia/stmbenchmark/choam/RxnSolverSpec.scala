60eee0e55e429917b82641453824585b732e5464
Daniel Urban <daniel.urban@nokia.com>
2024-04-04 17:40:14 +0200

    Print some JMX info after each test

--- choam/src/test/scala/com/nokia/stmbenchmark/choam/RxnSolverSpec.scala
+++ choam/src/test/scala/com/nokia/stmbenchmark/choam/RxnSolverSpec.scala
@@ -7,6 +7,7 @@
 package com.nokia.stmbenchmark
 package choam
 
+import cats.syntax.all._
 import cats.effect.IO
 
 import common.JvmCeIoSolverSpec
@@ -26,4 +27,29 @@ final class RxnSolverSpec extends JvmCeIoSolverSpec {
   testFromResource("sparselong_mini.txt")
   testFromResource("sparselong.txt")
   testFromResource("mainboard.txt", restrict = 3) // unrestricted takes approx. 12 mins
+
+  override def afterEach(context: AfterEach): Unit = {
+    printJmxInfo()
+  }
+
+  private def printJmxInfo(): Unit = {
+    val mbs = java.lang.management.ManagementFactory.getPlatformMBeanServer()
+    val names = mbs.queryNames(
+      new javax.management.ObjectName("dev.tauri.choam.internal.mcas.emcas:type=EmcasJmxStats*"),
+      null
+    )
+    names.forEach { objName =>
+      def getAttr(attrName: String): Option[AnyRef] =
+        Either.catchOnly[javax.management.AttributeNotFoundException](mbs.getAttribute(objName, attrName)).toOption
+      (
+        getAttr("Commits"),
+        getAttr("AvgCyclesPerCommit"),
+        getAttr("CyclesDetected")
+      ).mapN { (commits, avgCyclesPc, cyclesDetected) =>
+        println(s"  Commits =            ${commits}")
+        println(s"  AvgCyclesPerCommit = ${avgCyclesPc}")
+        println(s"  CyclesDetected =     ${cyclesDetected}")
+      }
+    }
+  }
 }
