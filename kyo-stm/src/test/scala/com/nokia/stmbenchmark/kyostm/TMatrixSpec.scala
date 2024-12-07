04447c6063ea026d749867dd54e3c99e20d0de51
Daniel Urban <daniel.urban@nokia.com>
2024-12-07 15:03:05 +0100

    Tests

---
+++ kyo-stm/src/test/scala/com/nokia/stmbenchmark/kyostm/TMatrixSpec.scala
@@ -0,0 +1,27 @@
+/*
+ * © 2023-2024 Nokia
+ * Licensed under the Apache License 2.0
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package com.nokia.stmbenchmark
+package kyostm
+
+import kyo.{ Async, Abort, STM }
+
+import munit.FunSuite
+
+final class TMatrixSpec extends FunSuite with KyoInterop {
+
+  testKyo("test1") {
+    for {
+      m <- STM.run(TMatrix(4, 4, "foo").map { m =>
+        m.set(1, 1, "bar").map(_ => m)
+      })
+      v01 <- STM.run(m(0, 1))
+      _ <- this.assertKyo(v01 == "foo")
+      v11 <- STM.run(m(1, 1))
+      _ <- this.assertKyo(v11 == "bar")
+    } yield ()
+  }
+}
